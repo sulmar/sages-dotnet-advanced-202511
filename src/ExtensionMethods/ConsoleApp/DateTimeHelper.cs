ce2b16eaa1d1220d2d9967137df468525a03bf8d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-04 17:27:49 +0100

    Dodanie metody rozszerzajacej

---
+++ src/ExtensionMethods/ConsoleApp/DateTimeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Helpers;
+
+internal static class DateTimeHelper
+{
+    public static bool IsWeekend(this DateTime date)
+    {
+        return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+    }
+
+    public static DateTime AddWorkDays(this DateTime date, int workDays)
+    {
+        return date.AddDays(workDays);
+    }
+}
+
+static class StringExtensions
+{
+    public static string RemovePolishLetters(this string text)
+    {
+        return text.Replace("©", "");
+    }
+}
---
+++ src/ExtensionMethods/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp;
+using ConsoleApp.Helpers;
+
+Console.WriteLine("Hello, World!");
+
+
+if (DateTime.Today.IsWeekend())
+{
+    Console.WriteLine("juz weekend!");
+}
+else
+    Console.WriteLine("dzien roboczy ©.".RemovePolishLetters());
+
+
+Console.WriteLine( DateTime.Today.AddWorkDays(2));
+
+
