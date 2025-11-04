ccd8c978e1ca12aa875584810f0737dc8e96fc2f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-04 11:16:03 +0100

    Dodanie przykladu wspolnego interfejsu

--- src/GenericTypes/GenericMethods/ListHelper.cs
+++ src/GenericTypes/GenericMethods/ListHelper.cs
@@ -16,7 +16,7 @@ public abstract class BaseEntity : Base
     public int Id { get; set; }
 }
 
-public class Customer : BaseEntity
+public class Customer : BaseEntity, IName
 {
     public string Name { get; set; }
 }
@@ -28,15 +28,32 @@ public class User : BaseEntity
     public string? Email { get; set; }
 }
 
+public class Document : BaseEntity, IName
+{
+    public string Name { get; set; }
+}
+
+public interface IName
+{
+    public string Name { get; set; }
+}
+
 internal class ListHelper
 {
     // Szablon metody (Metoda generyczna)
     public static void Dump<TItem>(IEnumerable<TItem> collection)
+        where TItem : Base
     {
         foreach (TItem item in collection)
         {
             Console.WriteLine(item);
         }
+    }
+
+    public static string NameToUpper<T>(T item)
+        where T : IName
+    {
+        return item.Name.ToUpper();
     }
      
 }
