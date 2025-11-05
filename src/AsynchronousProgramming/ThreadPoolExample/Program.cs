99a35b387c840da67d87c8c2e1b2d04ce5573e4d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-05 11:35:46 +0100

    Dodanie puli watkow

--- src/AsynchronousProgramming/ThreadPoolExample/Program.cs
+++ src/AsynchronousProgramming/ThreadPoolExample/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ThreadPoolExample;
+
+Console.WriteLine("Hello, Thread Pool!");
+
+
+Console.WriteLine("Started".DumpThreadId());
+
+EmailMessageService service = new EmailMessageService();
+
+//ThreadPool.QueueUserWorkItem(_ => service.Send($"john@domain.com"));
+//ThreadPool.QueueUserWorkItem(_ => service.Send($"kate@domain.com"));
+//ThreadPool.QueueUserWorkItem(_ => service.Send($"bob@domain.com"));
+
+//foreach(var i in Enumerable.Range(1, 100))
+//{
+//    ThreadPool.QueueUserWorkItem(_ => service.Send($"john{i}@domain.com"));
+//}
+
+var threads = Enumerable.Range(1, 100)
+        .Select(i => ThreadPool.QueueUserWorkItem(_ => service.Send($"john{i}@domain.com")))
+        .ToList();
+
+// new Thread(() => service.Send($"john{i}@domain.com")
+
+
+Console.WriteLine("Finished".DumpThreadId());
+Console.WriteLine("Press Enter key to exit.".DumpThreadId());
+
+
+Console.ReadLine();
