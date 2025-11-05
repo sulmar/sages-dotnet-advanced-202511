a023963ef6e9f095af89a2c63e4db75fb6aea6c3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-05 11:35:37 +0100

    Dodanie watkow

--- src/AsynchronousProgramming/ThreadsExample/Program.cs
+++ src/AsynchronousProgramming/ThreadsExample/Program.cs
@@ -7,26 +7,14 @@ Console.WriteLine("Started".DumpThreadId());
 
 EmailMessageService service = new EmailMessageService();
 
-Thread thread1 = new Thread(() => service.Send("john@domain.com"));
-Thread thread2 = new Thread(() => service.Send("bob@domain.com"));
-Thread thread3 = new Thread(() => service.Send("kate@domain.com"));
+var threads = Enumerable.Range(1, 100)
+        .Select(i => new Thread(() => service.Send($"john{i}@domain.com")));
 
-thread1.IsBackground = false;
-thread2.IsBackground = false;
-thread3.IsBackground = false;
+foreach (var thread in threads)
+{
+    thread.Start();
 
-Thread.Sleep(1000);
-
-thread1.Start();
-thread2.Start();
-
-Thread.Sleep(2000);
-
-thread3.Start();
-
-// service.Send("bob@domain.com");
-
-// service.Send("kate@domain.com");
+}
 
 Console.WriteLine("Finished".DumpThreadId());
 Console.WriteLine("Press Enter key to exit.".DumpThreadId());
