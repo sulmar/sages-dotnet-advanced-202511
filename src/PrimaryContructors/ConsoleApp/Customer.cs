22b461a78a74c202f2a6a531541581972ee8b1a8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-05 17:03:27 +0100

    Dodanie PrimaryConstructor

---
+++ src/PrimaryContructors/ConsoleApp/Customer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp;
+
+internal class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public byte Age { get; set; }
+    public string Pesel { get; set; }
+}
+
+interface ICustomerRepository
+{
+    Customer Get(int id);
+}
+
+class DbContext
+{
+
+}
+
+class DbSet<T> : IEnumerable<T>
+{
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class MyContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+}
+
+// Primary Constructor
+class DbCustomerRepository(MyContext context) : ICustomerRepository
+{
+    //private MyContext context;
+
+    //public DbCustomerRepository(MyContext context)
+    //{
+    //    this.context = context;
+    //}
+
+    public Customer Get(int id)
+    {
+        return context.Customers.Single(c => c.Id == id);
+    } 
+}
+
