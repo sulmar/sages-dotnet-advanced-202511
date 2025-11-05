2a037bea8a49d90b94dcf7478d7b7fd5549be9e6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-05 11:35:57 +0100

    Dodanie taskow

---
+++ src/AsynchronousProgramming/TasksExample/EmailMessageService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksExample;
+
+internal class EmailMessageService
+{
+    public void Send(string to)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        Console.WriteLine($"Sending message to {to}...".DumpThreadId());
+        File.AppendAllText($"log-{to}.txt", $"Sending message to {to}...".DumpThreadId());
+
+        // TODO: Simulate delay from 1 to 3 s. 
+        Thread.Sleep(Random.Shared.Next(3_000, 10_000));
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Sent to {to}. Elapsed time {stopwatch.Elapsed}".DumpThreadId());
+        File.AppendAllText($"log-{to}.txt", $"Sent to {to}...".DumpThreadId());        
+    }
+}
--- src/AsynchronousProgramming/TasksExample/Program.cs
+++ src/AsynchronousProgramming/TasksExample/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Threading.Tasks;
+using TasksExample;
+
+Console.WriteLine("Hello, Task!");
+
+Console.WriteLine("Started".DumpThreadId());
+
+EmailMessageService service = new EmailMessageService();
+
+//Task task1 = new Task(() => service.Send("john@domain.com"));
+//Task task2 = new Task(() => service.Send("john@domain.com"));
+//Task task3 = new Task(() => service.Send("john@domain.com"));
+
+// Zamiast
+//Task task1 = new Task(() => service.Send("john@domain.com"));
+// task1.Start();
+
+// Mozemy uzyc krocej fabryki (wzorzec projektowy Fabryka - metoda fabrykujaca) taskow
+Task task1 = Task.Factory.StartNew(() => service.Send("johnA@domain.com"), TaskCreationOptions.LongRunning);
+
+// Jeszcze krocej (pod spodem uzywa powyzszej fabryki)
+Task task2 = Task.Run(() => service.Send("johnB@domain.com"));
+
+// Czekamy na zakonczenie pojedynczego zadania
+// task1.Wait();
+
+// Czekamy na zakonczenie wielu zadan
+Task.WaitAll(task1, task2);
+
+var tasks = Enumerable.Range(1, 100)
+        .Select(i => new Task(() => service.Send($"john{i}@domain.com")))
+        .ToList();
+
+Thread.Sleep(1000);
+
+foreach (var task in tasks)
+{
+    task.Start();
+}
+
+Console.WriteLine("Finished".DumpThreadId());
+Console.WriteLine("Press Enter key to exit.".DumpThreadId());
+
+
+Console.ReadLine();
