ea50d8a929f0f0ac29b47129b367a4985fc1797a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-05 16:54:44 +0100

    Dodanie var

---
+++ src/AnonymousTypes/ConsoleApp/Customer.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp;
+
+interface ICustomerRepository
+{
+    Customer Get(int id);
+}
+
+class FakeCustomerRepository : ICustomerRepository
+{
+    public Customer Get(int id)
+    {
+        return new Customer { Id = 1, FirstName = "John", LastName = "Smith", Age = 30, Pesel = "123" };
+    }
+
+    public void Add(Customer customer)
+    {
+
+    }
+}
+
+class DbCustomerRepository : ICustomerRepository
+{
+    public Customer Get(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public byte Age { get; set; }
+    public string Pesel { get; set; }    
+}
+
+// Typ anonimowy
+public class AnonymousType_5cn85375n83758347589734985734987534987
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public byte Age { get; set; }
+
+}
+
+//public class CustomerInfo
+//{
+//    public string FirstName { get; set; }
+//    public string LastName { get; set; }
+//    public byte Age { get; set; }
+//}
+
+//public class CustomerInfo2
+//{
+//    public string FullName { get; set; }
+//    public byte Age { get; set; }
+//}
---
+++ src/AnonymousTypes/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using ConsoleApp;
+
+Console.WriteLine("Hello, Anonymous Types!");
+
+decimal x = 10.0m; // wnioskowanie typu na podst. przypisanej wartosci
+
+var customer1 = new Customer { Id = 1, FirstName = "John", LastName = "Smith", Age = 30, Pesel = "123" };
+var customer2 = new Customer { Id = 2, FirstName = "Bob", LastName = "Smith", Age = 30, Pesel = "123" };
+var customer3 = new Customer { Id = 3, FirstName = "Kate", LastName = "Smith", Age = 30, Pesel = "123" };
+
+List<Customer> customers = new List<Customer> { customer1, customer2, customer3 };
+
+var query = customers.Select(c => new { c.FirstName, c.LastName, c.Age });
+
+var customerInfo = new { FirstName = "John", LastName = "Smith", Age = 30 };
+
+Console.WriteLine(customerInfo.Age);
+
+var customerInfo2 = new { FullName = "John Smith", Age = 30 };
+
+ICustomerRepository customerRepository = new FakeCustomerRepository();
+var c = customerRepository.Get(1);
+// customerRepository.Add(new Customer());
+
+
+
+
