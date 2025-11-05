f39d4758aa5de023cb132081e0ec8430366fe002
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-05 09:44:07 +0100

    Dodanie przykladu

---
+++ src/AsynchronousProgramming/ThreadsExample/EmailMessageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsExample;
+
+internal class EmailMessageService
+{
+    public void Send(string to)
+    {
+        // TODO: Simulate delay from 1 to 3 s. 
+        
+        
+
+    }
+}
