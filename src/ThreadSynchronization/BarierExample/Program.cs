506a8659f49769bf360281bbd01547e3c9bc245e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 16:44:53 +0100

    Dodanie przykladu aktualizacji

---
+++ src/ThreadSynchronization/BarierExample/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BarierExample;
+
+Console.WriteLine("Hello, Barier!");
+
+
+Console.WriteLine("Rozpoczecie aktualizacji...");
+
+UpdateService updateService = new UpdateService();
+
+new Thread(updateService.Machine1).Start();
+new Thread(updateService.Machine2).Start();
---
+++ src/ThreadSynchronization/BarierExample/UpdateService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarierExample;
+
+internal class UpdateService
+{
+
+    public void Machine1()
+    {
+        Console.WriteLine("Machine1: Aktualizacja Windows...");
+        Thread.Sleep(10_000);
+        Console.WriteLine("Machine1: Aktualizacja Windows. Zakonczono.");
+
+        Console.WriteLine("Machine1: Instalacja VS");
+        Thread.Sleep(5000);
+        Console.WriteLine("Machine1: Instalacja VS. Zakonczono");
+
+        Console.WriteLine("Machine1: Weryfikacja instalacji");
+        Thread.Sleep(5000);
+        Console.WriteLine("Machine1: Weryfikacja instalacji. Zakonczono.");
+    }
+
+
+    public void Machine2()
+    {
+        Console.WriteLine("Machine2: Aktualizacja Windows...");
+        Thread.Sleep(1_000);
+        Console.WriteLine("Machine2: Aktualizacja Windows. Zakonczono.");
+
+        Console.WriteLine("Machine2: Instalacja VS");
+        Thread.Sleep(2000);
+        Console.WriteLine("Machine2: Instalacja VS. Zakonczono");
+
+        Console.WriteLine("Machine2: Weryfikacja instalacji");
+        Thread.Sleep(1000);
+        Console.WriteLine("Machine2: Weryfikacja instalacji. Zakonczono.");
+    }
+}
