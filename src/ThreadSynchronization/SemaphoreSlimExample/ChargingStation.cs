30752c5a5869daf30ed4af49b4ea8a8fb495c293
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 15:41:47 +0100

    Dodanie SemaphoreSlim

---
+++ src/ThreadSynchronization/SemaphoreSlimExample/ChargingStation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemaphoreSlimExample;
+
+internal class ChargingStation
+{
+    private readonly SemaphoreSlim _semaphore;
+
+    public ChargingStation(int maxConcurrentCharges)
+    {
+        _semaphore = new SemaphoreSlim(3, 3);
+    }
+
+    public async Task ChargeVehicleAsync(int vehicleId)
+    {
+        await _semaphore.WaitAsync(); // <--- pobieramy zeton lub czekamy jesli nie jest dostepny
+
+        Console.WriteLine($"Pojazd {vehicleId} rozpoczal ladowanie...");
+
+        // Symulacja czasu ladowania
+        await Task.Delay(Random.Shared.Next(5000, 10_000));
+
+        Console.WriteLine($"Pojazd {vehicleId} zakonczyl ladowanie.");
+
+        _semaphore.Release();  // <--- oddajemy zeton
+    }
+}
--- src/ThreadSynchronization/SemaphoreSlimExample/Program.cs
+++ src/ThreadSynchronization/SemaphoreSlimExample/Program.cs
@@ -2,6 +2,21 @@
 
 Console.WriteLine("Hello, Semaphore Slim!");
 
+var station = new ChargingStation(3);
+
+var tasks = Enumerable.Range(1, 10)
+    .Select(id => station.ChargeVehicleAsync(id))
+    .ToArray();
+
+await Task.WhenAll(tasks);
+
+
+Console.WriteLine("Wszystkie pojazdy zostaly naladowane.");
+
+
+
+return;
+
 RequestLimiter requestLimiter = new RequestLimiter(3); // TODO: Limit to 3 concurrent requests
 
 for (int i = 1; i <= 20; i++)
