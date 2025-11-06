83854d40b67602cc62d2fe975660357573676b05
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 08:43:59 +0100

    Dodanie BlockingCollections

---
+++ src/ConcurrentCollections/BlockingCollectionExample/Helper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockingCollectionExample;
+
+internal class Helper
+{
+    public static IEnumerable<string> GetWeekdays()
+    {
+        // Leniwa kolekcja (Lazy Collection)
+        yield return "Pn";
+        yield return "Wt";
+        yield return "Sr";
+        yield return "Cz";
+        yield return "Pt";
+        yield return "Sb";
+        yield return "Nd";
+    }
+
+    public static IEnumerable<string> Infinity()
+    {
+        // Leniwa kolekcja (Lazy Collection)
+        while (true)
+        {
+            Thread.Sleep(1000);
+            yield return "Sb";
+        }
+
+    }
+}
