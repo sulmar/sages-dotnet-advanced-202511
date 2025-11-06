73655735a34c8aaa9b16c795698c4472c9ca030c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 15:21:11 +0100

    Dodanie przykladu ladowania pojazdow

---
+++ src/ThreadSynchronization/SemaphoreExample/ChargingStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemaphoreExample;
+
+internal class ChargingStation
+{
+    public ChargingStation(int maxConcurrentCharges)
+    {
+        
+    }
+
+    public void ChargeVehicle(int vehicleId)
+    {
+        Console.WriteLine($"Pojazd {vehicleId} rozpoczal ladowanie...");
+
+        // Symulacja czasu ladowania
+        Thread.Sleep(Random.Shared.Next(2000, 4000));
+
+        Console.WriteLine($"Pojazd {vehicleId} zakonczyl ladowanie.");
+    }
+}
---
+++ src/ThreadSynchronization/SemaphoreExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using SemaphoreExample;
+
+Console.WriteLine("Hello, Semaphore!");
+
+var station = new ChargingStation(3);
+
+var tasks = Enumerable.Range(1, 10)
+    .Select(id => Task.Run(() => station.ChargeVehicle(id)))
+    .ToArray();
+
+await Task.WhenAll(tasks);
+
+
+Console.WriteLine("Wszystkie pojazdy zostaly naladowane.");
