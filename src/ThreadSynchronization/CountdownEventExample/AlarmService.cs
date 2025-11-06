4d4b0ff2bdeaf2acf02ffbb5b414bb83522c8bbc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 17:19:59 +0100

    Dodanie przykladu z alarmami

---
+++ src/ThreadSynchronization/CountdownEventExample/AlarmService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountdownEventExample;
+
+class AlarmService(int count)
+{
+    private readonly CountdownEvent countdown = new CountdownEvent(count);
+
+    public void RaiseAlarm(string source)
+    {
+        Console.WriteLine($"{source}: Wykryto alarm!");
+        Thread.Sleep(Random.Shared.Next(500, 1500)); 
+        countdown.Signal(); 
+
+        Console.WriteLine($"{source}: Alarm zgłoszony ({countdown.CurrentCount} pozostało).");
+    }
+
+    public void WaitForAllAlarms()
+    {
+        countdown.Wait(); // czeka aż wszystkie alarmy zostaną zgłoszone
+                          
+        
+    }
+
+    
+
+}
--- src/ThreadSynchronization/CountdownEventExample/Program.cs
+++ src/ThreadSynchronization/CountdownEventExample/Program.cs
@@ -3,6 +3,8 @@ using CountdownEventExample;
 
 Console.WriteLine("Hello, CountdownEvent!");
 
+AlarmTest();
+
 ParkingGate parkingGate = new ParkingGate(10);
 
 var tasks = Enumerable.Range(1, 10)
@@ -16,9 +18,22 @@ Console.WriteLine("Parking pusty");
 
 await Task.WhenAll(tasks);
 
+static void AlarmTest()
+{    
+    var service = new AlarmService(3);
 
+    new Thread(() => service.RaiseAlarm("Czujnik A")).Start();
+    new Thread(() => service.RaiseAlarm("Czujnik B")).Start();
+    new Thread(() => service.RaiseAlarm("Czujnik C")).Start();
 
+    // Główny wątek czeka 3 alarmy
+    service.WaitForAllAlarms();
 
+    // Wysyla zbiorczy raport
+    SendReport();
+}
 
-
-
+static void SendReport()
+{
+    Console.WriteLine("Raport wysłany do administratora.");
+}
