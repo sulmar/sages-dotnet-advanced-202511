f28d33a6755e48fbdd96a765e9224cfaead62095
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 15:42:00 +0100

    Dodanie Semaphore

--- src/ThreadSynchronization/SemaphoreExample/ChargingStation.cs
+++ src/ThreadSynchronization/SemaphoreExample/ChargingStation.cs
@@ -8,18 +8,24 @@ namespace SemaphoreExample;
 
 internal class ChargingStation
 {
+    private readonly Semaphore _semaphore;
+
     public ChargingStation(int maxConcurrentCharges)
     {
-        
+        _semaphore = new Semaphore(3, 3);
     }
 
     public void ChargeVehicle(int vehicleId)
     {
+        _semaphore.WaitOne(); // <--- pobieramy zeton lub czekamy jesli nie jest dostepny
+
         Console.WriteLine($"Pojazd {vehicleId} rozpoczal ladowanie...");
 
         // Symulacja czasu ladowania
-        Thread.Sleep(Random.Shared.Next(2000, 4000));
+        Thread.Sleep(Random.Shared.Next(5000, 10_000));
 
         Console.WriteLine($"Pojazd {vehicleId} zakonczyl ladowanie.");
+
+        _semaphore.Release();  // <--- oddajemy zeton
     }
 }
