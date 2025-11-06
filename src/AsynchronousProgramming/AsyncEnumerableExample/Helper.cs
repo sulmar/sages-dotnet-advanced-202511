a8486b835b156159bcc33acaca969bc9eb93bc57
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-06 09:27:29 +0100

    Dodanie przykladu

---
+++ src/AsynchronousProgramming/AsyncEnumerableExample/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncEnumerableExample;
+
+internal class Helper
+{
+    // Leniwa kolekcja
+    public static IEnumerable<string> GetWeekdays()
+    {
+        yield return GetIcon("pn");
+        yield return GetIcon("wt");
+        yield return GetIcon("sr");
+        yield return GetIcon("cz");
+        yield return GetIcon("pt");
+        yield return GetIcon("sb");
+        yield return GetIcon("nd");
+    }
+
+    private static string GetIcon(string weekday)
+    {    
+        Thread.Sleep(1000);
+
+        return $"{weekday}.png";
+    }
+}
---
+++ src/AsynchronousProgramming/AsyncEnumerableExample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsyncEnumerableExample;
+
+Console.WriteLine("Hello, Async Enumerable!".DumpThreadId());
+
+
+foreach(var weekday in Helper.GetWeekdays())
+{
+    Task.Run(()=> Console.WriteLine(weekday.DumpThreadId()));
+}
+
+
+Console.WriteLine("Press Enter to exit.".DumpThreadId());
+Console.ReadLine();
