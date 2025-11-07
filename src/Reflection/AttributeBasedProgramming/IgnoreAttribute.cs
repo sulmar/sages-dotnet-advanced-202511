5d5f1dbb5b8dcb7f97bba4a6d49cc5fead4f262c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-07 13:36:58 +0100

    Dodanie AttributeUsage

--- src/Reflection/AttributeBasedProgramming/IgnoreAttribute.cs
+++ src/Reflection/AttributeBasedProgramming/IgnoreAttribute.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace AttributeBasedProgramming;
 
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 internal class IgnoreAttribute : Attribute
 {
 }
