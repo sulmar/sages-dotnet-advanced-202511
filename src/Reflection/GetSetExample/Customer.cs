fcaccc3d76c5795689405d4d07a01c187b03b494
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-07 12:22:56 +0100

    Pobieranie i ustawianie wartosci

--- src/Reflection/GetSetExample/Customer.cs
+++ src/Reflection/GetSetExample/Customer.cs
@@ -1,8 +1,46 @@
-﻿namespace GetSetExample;
+﻿using System.Reflection;
+
+namespace GetSetExample;
 
 public class Customer
-{    
+{
     public string Name { get; set; }
     public string Email { get; set; }
+    public byte Age { get; set; }
 
+    // Wlasny indekser [propertyName]
+    /*
+    public object this[string propertyName]
+    {
+        get
+        {
+            Type type = this.GetType();
+
+            PropertyInfo property = type.GetProperty(propertyName);
+
+            object value = property.GetValue(this);
+
+            return value;
+
+        }
+
+        set
+        {
+            Type type = this.GetType();
+
+            PropertyInfo property = type.GetProperty(propertyName);
+
+            property.SetValue(this, value);
+
+        }
+    }
+
+    */
+
+
+    public object this[string propertyName]
+    {
+        get => GetType().GetProperty(propertyName).GetValue(this);
+        set => GetType().GetProperty(propertyName).SetValue(this, value);
+    }
 }
--- src/Reflection/GetSetExample/Program.cs
+++ src/Reflection/GetSetExample/Program.cs
@@ -1,10 +1,32 @@
 ﻿using GetSetExample;
+using System.Reflection;
 
 Console.WriteLine("Hello, Reflection!");
 
-Customer customer = new Customer { Name = "John Doe", Email = "john.doe@example.com" };
+Customer customer = new Customer { Name = "John Doe", Email = "john.doe@example.com", Age = 30 };
 
+Console.Write("Podaj nazwe wlasciwosci: ");
+string? propertyName = Console.ReadLine();
+
+if (propertyName != null)
+{
+    Type type = customer.GetType();
+
+    PropertyInfo property = type.GetProperty(propertyName);
+
+    // Odczyt wartosci
+    object value = property.GetValue(customer);
+
+    Console.WriteLine(value);
+
+    // Zapis wartosci
+    property.SetValue(customer, "John Smith");
+
+    Console.WriteLine(customer.Name);
+
+}
 
 // TODO:  Przypisz wartości do właściwości za pomocą indeksu
-// customer["Name"] = "John Doe";
-// customer["Email"] = "john.doe@example.com";
+customer[propertyName] = "John Doe";
+Console.WriteLine(customer[propertyName]);
+
